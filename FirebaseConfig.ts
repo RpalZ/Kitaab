1a5c007bef2ab9cd5c26d0ab095f7e37d5a50e4e
RpalZ <72819391+RpalZ@users.noreply.github.com>
2025-01-19 20:50:34 +0300

    Update FirebaseConfig.ts

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
   projectId: FIREBASE_PROJECT_ID,
   storageBucket: FIREBASE_STORAGE_BUCKET,
