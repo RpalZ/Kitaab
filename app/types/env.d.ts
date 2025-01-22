fda04a8faf0e120cdc2d438cab42d158603a0634
RpalZ <72819391+RpalZ@users.noreply.github.com>
2025-01-22 18:59:19 +0300

    applied the api lolzies

--- app/types/env.d.ts
+++ app/types/env.d.ts
@@ -7,5 +7,6 @@ declare module '@env' {
   export const FIREBASE_MESSAGING_SENDER_ID: string;
   export const FIREBASE_APP_ID: string;
   export const FIREBASE_MEASUREMENT_ID: string;
+  export const DEEPSEEK_API_KEY: string;
 
 } 
