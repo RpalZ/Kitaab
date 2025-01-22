9ee46c81325916341253d8aa9e4453e774652dab
maaz_khan <poketrainer5645@gmail.com>
2025-01-22 18:32:40 +0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,6 +20,7 @@ import { ActivityIndicator, LogBox } from "react-native";
 
 
 import { secureStorage } from "./utils/secureStorage";
+import ClassDetail from "./teacher/class/[id]";
 
 LogBox.ignoreLogs([
   "Warning: ...", // Add specific warning messages here
@@ -91,9 +92,10 @@ export default function RootLayout() {
       <Stack.Screen
         name="student/dashboard"
         options={{ title: "Student Dashboard" }}
+      />
+      <Stack.Screen
         name="teacher/class/[id]"
         options={{ title: "Class Details" }}
-        // component={ClassDetail}
       />
       <Stack.Screen 
         name="student/profile" 
